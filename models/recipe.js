72f3411db3cec68eda57e736fe092ab18285e8a6
Björn Tirsén <bjorntirsen@gmail.com>
2021-03-05 08:31:31 +0100

    ✨Add to recipe schema

--- models/recipe.js
+++ models/recipe.js
@@ -1,15 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    maxlength: 100
+    maxlength: 100,
   },
   for: {
     type: Number,
     required: true,
     min: 1,
-    max: 36
-  }
-})
+    max: 36,
+  },
+  minutes: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 360,
+  },
+  method: {
+    type: String,
+    required: true,
+    maxlength: 10000,
+  },
+  ingredients: [
+    {
+      i_name: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+      },
+      i_amount: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10000,
+      },
+      i_unit: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('Recipie', recipeSchema);
